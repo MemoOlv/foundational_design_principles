ca6fad233ee64d01fe066f4e4b0467ba218a7c19
MemoOlv <guillermo.olvera@islas.org.mx>
2025-01-23 11:13:09 -0600

    🛑🧪 Fail tests

--- tests/test_polymorphism.py
+++ tests/test_polymorphism.py
@@ -1,8 +1,12 @@
-from src.polymorphism import CreditCard
+from src.polymorphism import CreditCard, Paypal
 
 def test_payment_methods():
     amount_to_pay = 100
     credit_card_payment = CreditCard(amount_to_pay)
     obtained = credit_card_payment.process_payment()
     assert obtained == "Credit card payment for 100: succesfull"
+
+    paypal_payment = PayPal(amount_to_pay)
+    obtained = paypal_payment.process_payment()
+    assert obtained == "Paypal payment for 100: succesfull"
 
