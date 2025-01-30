f8b8db0725f56d77dade0acc19202f47f27739cd
MemoOlv <guillermo.olvera@islas.org.mx>
2025-01-30 11:03:48 -0600

    🧪 Add failing test

--- tests/test_composition.py
+++ tests/test_composition.py
@@ -3,9 +3,9 @@ from src.composition import Car, Plane
 
 def test_car_with_engine():
     obtained = Car()
-    assert obtained.start() == "Engine started"
+    assert obtained.start() == "Engine started; Car started"
 
 
 def test_plane_with_engine():
     obtained = Plane()
-    assert obtained.start() == "Engine started"
+    assert obtained.start() == "Engine started; Plane started"
