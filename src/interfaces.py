567dd8057e8d6964978ea6188ab205d58c0d2f77
MemoOlv <guillermo.olvera@islas.org.mx>
2025-02-05 11:15:32 -0600

    ✅ Example of abstract class

---
+++ src/interfaces.py
@@ -0,0 +1,12 @@
+from abc import ABC, abstractmethod
+
+
+class MyInterface(ABC):
+    @abstractmethod
+    def do_something(self, param: str):
+        pass
+
+
+class MyClass(MyInterface):
+    def do_something(self, param: str):
+        return f"Doing something with: {param}"
