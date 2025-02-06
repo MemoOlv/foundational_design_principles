6f98931f2b30acbc55191a35a90ad3cfe18bd8ea
MemoOlv <guillermo.olvera@islas.org.mx>
2025-02-06 11:17:28 -0600

    ✅ Pass test

--- src/interfaces.py
+++ src/interfaces.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+import pathlib
 
 
 class MyInterface(ABC):
@@ -21,6 +22,15 @@ class ConsoleLogger(Logger):
     def log(self, message: str):
         return f"Console: {message}"
 
+class FileLogger(Logger):
+    def __init__(self, file_path: str):
+        self.file = pathlib.Path(file_path)
+
+    def log(self, message: str):
+        if not self.file.exists():
+            self.file.touch()
+        self.file.write_text(f"File: {message}")
+        return self.file.read_text()
 
 def log_message(logger: Logger, message: str):
     return logger.log(message)
