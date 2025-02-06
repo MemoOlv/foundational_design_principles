056a4c1e161b15598d80d0a62249c7802106c685
MemoOlv <guillermo.olvera@islas.org.mx>
2025-02-06 10:56:52 -0600

    🧪 Add failing test

--- tests/test_interfaces.py
+++ tests/test_interfaces.py
@@ -1,4 +1,4 @@
-from src.interfaces import MyClass, ConsoleLogger
+from src.interfaces import MyClass, ConsoleLogger, log_message
 
 def test_class_as_interface():
     myclass = MyClass()
@@ -9,4 +9,7 @@ def test_class_as_interface():
 
 
 def test_loggers():
-    ConsoleLogger()
+    console_log_message = "A console log"
+    console_logger = ConsoleLogger()
+    obatined_console_logger = log_message(console_logger, console_log_message)
+    assert obatined_console_logger == "Console: A console log"
