46189df01b2774f937b6f721ae5bcce649b52a6c
MemoOlv <guillermo.olvera@islas.org.mx>
2025-02-18 11:17:36 -0600

    🎨 Format code

--- src/dependency_injection.py
+++ src/dependency_injection.py
@@ -1,13 +1,16 @@
 class MessageService:
     def __init__(self, sender):
         self.sender = sender
+
     def send_message(self, message: str):
         return self.sender.send(message)
+
 
 class EmailSender:
     def send(self, message: str):
         return f"Sending mail: {message}"
-    
+
+
 class SMSSender:
     def send(self, message: str):
         return f"Sending SMS: {message}"
--- tests/test_dependency_injection.py
+++ tests/test_dependency_injection.py
@@ -1,5 +1,6 @@
 from src.dependency_injection import MessageService, EmailSender, SMSSender
 
+
 def test_messager_service():
     email_service = MessageService(EmailSender())
     obtained = email_service.send_message("Hello via Email")
@@ -10,4 +11,3 @@ def test_messager_service():
     obtained = sms_service.send_message("Hello via SMS")
     expected_sms_message = "Sending SMS: Hello via SMS"
     assert obtained == expected_sms_message
-
