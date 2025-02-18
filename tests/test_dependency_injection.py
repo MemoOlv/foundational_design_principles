8e8dcc59f3efe413425a4a336da24272bc13d524
MemoOlv <guillermo.olvera@islas.org.mx>
2025-02-18 11:16:07 -0600

    🧪 Add failing test

--- tests/test_dependency_injection.py
+++ tests/test_dependency_injection.py
@@ -1,8 +1,13 @@
-from src.dependency_injection import MessageService, EmailSender
+from src.dependency_injection import MessageService, EmailSender, SMSSender
 
 def test_messager_service():
     email_service = MessageService(EmailSender())
     obtained = email_service.send_message("Hello via Email")
     expected_email_message = "Sending mail: Hello via Email"
     assert obtained == expected_email_message
+
+    sms_service = MessageService(SMSSender())
+    obtained = sms_service.send_message("Hello via SMS")
+    expected_sms_message = "Sending SMS: Hello via SMS"
+    assert obtained == expected_sms_message
 
